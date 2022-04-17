edc7933f407427bc2f0c95453c49be3e95786a33
Calvin Ben Hardi <calvinbenhardi@gmail.com>
2022-04-17 11:15:21 +0700

    Add Error struct for error response & ErrorJSON http response handler

--- res/json.go
+++ res/json.go
@@ -7,8 +7,25 @@ import (
 
 type Map map[string]interface{}
 
+type Error struct {
+	Code    int         `json:"code"`
+	Message interface{} `json:"message"`
+}
+
 func RenderJSON(w http.ResponseWriter, status int, body interface{}) {
 	w.WriteHeader(status)
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(&body)
+}
+
+func ErrorJSON(w http.ResponseWriter, status int, message interface{}) {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+
+	errorResponse := Error{
+		Code:    status,
+		Message: message,
+	}
+
+	json.NewEncoder(w).Encode(&errorResponse)
 }
